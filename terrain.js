3296b0a2a1dc9d350fffb6a0739c85f3aad5eb08
Zac Hegwood <zachegwood@gmail.com>
2025-06-10 09:59:30 -0400

    refactor into biomeBlend class

--- terrain.js
+++ terrain.js
@@ -4,6 +4,16 @@ import { myShaders } from './main.js';
 import { generateSimplexNoise,  fractalNoise, fractalNoiseRaw } from './simplexNoise.js';
 
 
+//#region Lerp / Smooth
+function lerp (a, b, t) {
+    return a + t * (b - a);
+}
+
+function smoothstep(edge0, edge1, x) {
+    const t = Math.max(0, Math.min(1, (x - edge0) / (edge1 - edge0)));
+    return t * t * (3 - 2 * t);
+}
+
 
 // ProcGen Terrain
 
@@ -26,7 +36,7 @@ function worldToChunkCoord(x,z) {
 //#region Build Terrain
 export function buildTerrain(gl) {
 
-    const CHUNK_PIECES = 50;
+    const CHUNK_PIECES = 25;
     const halfPieces = CHUNK_PIECES / 2;
     const texture = loadTexture(gl, "Art/testTile.png");
 
@@ -54,43 +64,72 @@ export function buildTerrain(gl) {
     return chunks;
 }
 
+//#region Biome Blender
+class BiomeBlender {
+    constructor(biomeData) {
+        this.plainsParams = biomeData.plains;
+        this.mountainsParams = biomeData.mountains;
+    }
 
-//#region Biomes
-// Low-frequency noise determines the biome type (0 = plains, 1 = mountains)
-function getBiomeValue(x,z) {
+    getBlendedNoise(x, z) {
+        const params = this.getInterpolatedParams(x,z);
+        return { noise: fractalNoise(params), amp: params.amp };
+    }
 
-    // 1) continent mask: huge landmass
-    const CONTINENT_FREQ = 0.0008;
-    const continentValue = (generateSimplexNoise(x * CONTINENT_FREQ, z * CONTINENT_FREQ) + 1) / 2;
+    getInterpolatedParams(x,z) {
 
-    // 2) biome mask: mountains vs plains
-    const BIOME_FREQ = 0.0015; // Very low frequency => large smooth regions
-    let biomeValue = generateSimplexNoise(x * BIOME_FREQ, z * BIOME_FREQ);
 
-    // const minFloor = (1 - continentValue) * 0.1;
-    // return minFloor + continentValue * (biomeValue - minFloor);
 
-    // 3) Blend continent into biome
-    // ex, 70% biome detail, 30% continent outline
-    const blendedValue = lerp(biomeValue, continentValue, 0.3); // continental influence. 0.1-05
+        // Get biome value at this position (0 = plains, 1 = mountains)
+        // allows smooth map variation
+        const biomeRaw = this.getBiomeValue(x, z);
+            
+            
+        let blend = smoothstep(0.3, 0.7, biomeRaw);
 
-    return blendedValue;
-    //return (blendedValue + 1) / 2;
+        
 
-    // Normalize from -1..1 to 0..1
-    //return (value + 1) / 2;
+        return {
+            x: x,
+            z: z,
+            octaves:    lerp(this.plainsParams.octaves, this.mountainsParams.octaves, blend),
+            lacunarity: lerp(this.plainsParams.lacunarity, this.mountainsParams.lacunarity, blend),
+            gain:       lerp(this.plainsParams.gain, this.mountainsParams.gain, blend),
+            freq:       lerp(this.plainsParams.freq, this.mountainsParams.freq, blend),
+            amp:        lerp(this.plainsParams.amp, this.mountainsParams.amp, blend),
+        }
+
+
+
+
+    }
+
+    getBiomeValue(x,z) {
+
+        // 1) continent mask: huge landmass
+        const CONTINENT_FREQ = 0.0008;
+        const continentValue = (generateSimplexNoise(x * CONTINENT_FREQ, z * CONTINENT_FREQ) + 1) / 2;
+
+        // 2) biome mask: mountains vs plains
+        const BIOME_FREQ = 0.0015; // Very low frequency => large smooth regions
+        let biomeValue = generateSimplexNoise(x * BIOME_FREQ, z * BIOME_FREQ);
+
+        // 3) Blend continent into biome
+        // ex, 70% biome detail, 30% continent outline
+        const blendedValue = lerp(biomeValue, continentValue, 0.3); // continental influence. 0.1-05
+
+        return blendedValue;
+    }
+
+
 
 }
 
-//#region Lerp
-function lerp (a, b, t) {
-    return a + t * (b - a);
-}
 
-function smoothstep(edge0, edge1, x) {
-    const t = Math.max(0, Math.min(1, (x - edge0) / (edge1 - edge0)));
-    return t * t * (3 - 2 * t);
-}
+
+
+
+
 
 //#region Biome Data
 const biomeData = {
@@ -111,66 +150,13 @@ const biomeData = {
 }
 
 
-//#region Biome Params
-function getBiomeParameters(biome) {
-
-    // // Define extremes
-    // const plains = {
-    //     octaves:    5,
-    //     lacunarity: 2.0,
-    //     gain:       0.5,
-    //     freq:       0.02,
-    //     amp:        4.0
-    // };
-
-    // const mountains = {
-    //     octaves:    5,
-    //     lacunarity: 2.0,
-    //     gain:       0.4,
-    //     freq:       0.01,
-    //     amp:        20.0
-    // };
-
-    const plains = biomeData.plains;
-    const mountains = biomeData.mountains;
-
-    // Interpolate
-    return {
-        x: 0, z: 0, // filled in later
-        octaves: lerp(plains.octaves, mountains.octaves, biome),
-        lacunarity: lerp(plains.lacunarity, mountains.lacunarity, biome),
-        gain:       lerp(plains.gain,       mountains.gain,       biome),
-        freq:       lerp(plains.freq,       mountains.freq,       biome),
-        amp:        lerp(plains.amp,        mountains.amp,        biome)
-    };
-}
-
-
-
-
-
-
 //#region Gen Flat Grid
 export function generateFlatGrid(width, depth, segmentsX, segmentsZ, offsetX, offsetZ) {
 
     const positions = [];
     const indices = [];
     const uvs = [];
-    const biomes = []; // used to send to shader
-
-    const scale = 0.1; // controls "zoom level" of noise  
-
-    // If you want “continents” or larger features, you might
-    // use a very low base scale (e.g. 0.001), then let fBm handle the rest.
-    // In practice, you can just multiply worldX/worldZ by a single base scale:
-    //const baseScale = 0.005;  // tweak until you see rolling hills rather than tiny bumps
-    //const amplitude = 20.0;   // overall vertical exaggeration (max height)
-        //  move this to below
-
-    // fBm parameters:
-    // const OCTAVES     = 5;
-    // const LACUNARITY  = 2.0;
-    // const GAIN        = 0.5;
+    const biomesArray = []; // used to send to shader
 
     for (let z = 0; z <= segmentsZ; z++) {
         for (let x = 0; x <= segmentsX; x++) {
@@ -181,115 +167,38 @@ export function generateFlatGrid(width, depth, segmentsX, segmentsZ, offsetX, of
             let worldX = posX + offsetX;
             let worldZ = posZ + offsetZ;
 
-            // Get biome value at this position (0 = plains, 1 = mountains)
-            // allows smooth map variation
-            const biomeRaw = getBiomeValue(worldX, worldZ);
-            biomes.push(biomeRaw);
+            //const newParams = biomeBlender.getInterpolatedParams(x,z);
+
+            const biomeBlender = new BiomeBlender(biomeData);
+
+
+
+
+            const result = biomeBlender.getBlendedNoise(worldX, worldZ);
+
+            let y = result.noise * result.amp;
+
             
-            let biome = smoothstep(0.3, 0.7, biomeRaw);
-
-            // const biomeParameters = getBiomeParameters(biome);
-            // biomeParameters.x = worldX;
-            // biomeParameters.z = worldZ;
-
-            //let noiseValue = fractalNoise(biomeParameters); // the actual algorithm
-
-
-            let plainsParams = {x: worldX, z: worldZ, ...biomeData.plains};
-            let mountainsParams = {x: worldX, z: worldZ, ...biomeData.mountains};
-
-
-            let plainsNoise = (fractalNoise(plainsParams));
-            let mountainsNoise = (fractalNoise(mountainsParams));
-            let noiseValue = lerp(plainsNoise, mountainsNoise, biome);
-            let blendedAmp = lerp(biomeData.plains.amp, biomeData.mountains.amp, biome);
             
-            let y = noiseValue * blendedAmp;
+            
+
+            // passed to shader for visualization. debug only
+            biomesArray.push(biomeBlender.getBiomeValue(worldX,worldZ));
 
 
-            // let normalized = (noiseValue + 1) * 0.5;
-            // let smoothed = Math.pow(normalized, 1.2); // flattens peaks
-            // let y = smoothed * biomeParameters.amp * 2 - biomeParameters.amp;
+            // let plainsParams = {x: worldX, z: worldZ, ...biomeData.plains};
+            // let mountainsParams = {x: worldX, z: worldZ, ...biomeData.mountains};
+            // let plainsNoise = (fractalNoise(plainsParams));
+            // let mountainsNoise = (fractalNoise(mountainsParams));
 
-
-            // choose noise parameters based on biome
-            // const plainsFreqency =      0.02;
-            // const mountainsFrequency =  0.005;
-            // const plainsAmp =           4.0;
-            // const mountainsAmp =        25.0;
-
-            // const frequency = lerp(plainsFreqency, mountainsFrequency, biome);
-            // const amplitude = lerp(plainsAmp, mountainsAmp, biome);
-
-   
-
-            // if (biome < 0.33) {
-
-            //     biomeParameters = {
-            //         x: worldX,
-            //         z: worldZ,
-            //         octaves: 4,
-            //         lacunarity: 2,
-            //         gain: 0.5,
-            //         freq: 0.02,
-            //         amp: 2.0
-            //     }
-
-            // } else if (biome < 0.66) {
-
-            //     biomeParameters = {
-            //         x: worldX,
-            //         z: worldZ,
-            //         octaves: 5,
-            //         lacunarity: 2.2,
-            //         gain: 0.55,
-            //         freq: 0.02,
-            //         amp: 6.0
-            //     }
-
-            // } else {
-
-            //     biomeParameters = {
-            //         x: worldX,
-            //         z: worldZ,
-            //         octaves: 6,
-            //         lacunarity: 2.5,
-            //         gain: 0.6,
-            //         freq: 0.01,
-            //         amp: 25.0
-            //     }
-
-            // }
-
-            // let noiseValue = fractalNoise(biomeParameters);
-
-            // Sample fractal noise at (worldX, worldZ):
-            // let noiseValue = fractalNoise(
-            //     worldX * baseScale,
-            //     worldZ * baseScale,
-            //     OCTAVES, LACUNARITY, GAIN,
-            //     frequency, amplitude
-            // );
-           // noiseValue ∈ approximately [−1 … +1]
-
-            // Optionally “lift” the range to [0…1] if it’s easier to shape:
-            // let normalized = (noiseValue + 1) * 0.5; // ∈ [0…1]
-            // Then you could e.g. apply a curve (normalized**1.2) to flatten plateaus.
-
-            //let y = noiseValue * amplitude;
-            //let y = noiseValue;
-            //let y = noiseValue * biomeParameters.amp;
-            //let y = generateSimplexNoise(worldX * scale, worldZ * scale) * amplitude;
-            //let y = Math.random() * 6;
-            //let y = biome * 10;
-
-            //y = biome * 30;
+            // let noiseValue = lerp(plainsNoise, mountainsNoise, biome);
+            // let blendedAmp = lerp(biomeData.plains.amp, biomeData.mountains.amp, biome);
+            
+            //let y = noiseValue * blendedAmp;
 
             positions.push(posX, y, posZ); 
-            //positions.push(posX, 0, posZ); // y = 0 for now
 
             uvs.push(x / segmentsX, z / segmentsZ);
-            //uvs.push(x / segmentsX, biome); // <- use biome as UV.v
         }
     }
 
@@ -308,7 +217,7 @@ export function generateFlatGrid(width, depth, segmentsX, segmentsZ, offsetX, of
         }
     }  
 
-    return { positions, indices, uvs, biomes };
+    return { positions, indices, uvs, biomes: biomesArray };
 
 }
 
